9f87038ca0de20557da6022a640f83f130994fcc
Laurizylinda <laurizy.santos@estudante.ifms.edu.br>
2025-09-17 11:49:28 -0400

    CRIALAO DE UM POST D E PRODUTOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type ProdutoType = {
+  _id: string;
+  nome: string;
+  preco: number;
+  urlfoto:string;
+  descricao: string;
+}
 
-  return (
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  const []
+  useEffect(() => {
+    fetch('/api/produtos')
+      .then(response => response.json())
+      .then(data => setProdutos(data));
+  }, []);
+   function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>Cadastro de Produtos<div/>
+    <form onSubmit={handleForm}>
+      <input type="text" name="nome" placeholder="Nome" />
+      <input type="number" name="preco" placeholder="Preço" />
+      <input type="text" name="urlfoto" placeholder="URL da Foto" />
+      <input type="text" name="descricao" placeholder="Descrição" />
+      <button type="submit">Adicionar Produto</button>
+    </form>
+    <div>Lista de produtos</div>
+
+      {produtos.map(produto => (
+        <div key={produto._id}>
+          <h2>{produto.nome}</h2>
+          <p>Preço: {produto.preco}</p>
+          <img src={produto.urlfoto} alt={produto.nome} />
+          <p>{produto.descricao}</p>
+        </div>
+      ))}
+    </div>
     </>
   )
 }
